bc377b62f7f6dbaf285365893cfce8e26a8619ac
Muhammad Lutfi Ashshidiq <luthfiashshidiq16@gmail.com>
2021-05-01 17:20:22 +0700

    Benerin CRUD

--- resources/views/produk_v.blade.php
+++ resources/views/produk_v.blade.php
@@ -12,7 +12,7 @@
     <div class="col-md-12">
       <div class="card">
         <div class="card-header">
-          {{ __('MANAGEMENT Product Setting') }}
+          {{ __('Product Setting') }}
 
         </div>
         <div class="card-body">
